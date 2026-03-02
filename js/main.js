8fc87aa145bea012077c6596201c1491c2188da8
GesundesWasser <git@wagger.dev>
2026-03-02 19:54:34 +0100

    fancy ahh sound

--- js/main.js
+++ js/main.js
@@ -15,4 +15,29 @@ import loadFooter from './footer.js';
 loadTheme();
 loadHeader();
 loadFooter();
+
+// Create audio instance
+const audio = new Audio('https://download.scamcraft.net/Soyuzed.mp3');
+audio.preload = "auto";
+audio.loop = false; // change to true if you want looping
+
+// Function to safely play audio
+function startAudio() {
+    audio.play()
+        .then(() => {
+            console.log("[krdb.info] Audio started successfully.");
+        })
+        .catch((err) => {
+            console.warn("[krdb.info] Autoplay blocked:", err);
+        });
+
+    // Remove listeners after first interaction
+    document.removeEventListener("click", startAudio);
+    document.removeEventListener("keydown", startAudio);
+}
+
+// Wait for first user interaction (required by browsers)
+document.addEventListener("click", startAudio, { once: true });
+document.addEventListener("keydown", startAudio, { once: true });
+
 console.log("[krdb.info] Successfully loaded all Scripts!");
