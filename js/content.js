f2c3ed890beafbb62f8d485ef9677dfb49c1bead
GesundesWasser <git@wagger.dev>
2026-03-02 19:55:58 +0100

    Last Changes

--- js/content.js
+++ js/content.js
@@ -2,105 +2,27 @@
 import jSecRenderer from './jSecRenderer.js';
 
 const SectionsMain = [
+      {
+        imgSrc: '', // Verbuggt
+        author: 'Comrade Sam',
+        date: '02.03.2026',
+        imgAlt: '', // Verbuggt
+        title: 'Day of Seal',
+        description: 'Halölölö? Heute ist der Day of Seal von Comrade Jakob, Es wurde der Seal of Yes! Er hat sich dazu entschieden, den Seal of No in KRDB hinter den Stall zu erschießen! Die Leiche wurde natürlich per Cargo-Rakete zum Dönerklotz angemeldet!',
+        buttonText: 'Lorem ipsum',
+        videoSrc: 'https://download.scamcraft.net/krdbvideo-01.mp4',
+        videoType: 'mp4',
+        showButton: false, // Verbuggt
+    },
     {
         imgSrc: '', // Verbuggt
         author: 'Comrade Sam',
-        date: '26.02.2026',
+        date: '02.03.2026',
         imgAlt: '', // Verbuggt
         title: 'Release von krdb.info',
         description: 'Halölölö? Hier ist Comrade Sam, Live aus Bulettien! Die neue Seite krdb.info ist jetzt offiziell online, das heißt jeder, auch außerhalb KRDB kann die News sehen!',
         buttonText: 'Lorem ipsum',
         showButton: false, // Verbuggt
-    },
-    {
-        imgSrc: '',
-        author: 'Leonidas Vanec, Ilse Bamberg',
-        date: '26.02.2026',
-        imgAlt: '',
-        title: 'Lorem ipsum dolor sit amet, consetetur sadipscing',
-        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-        buttonText: 'Lorem ipsum',
-        showButton: false,
-    },
-    {
-        imgSrc: '',
-        author: 'Leonidas Vanec, Ilse Bamberg',
-        date: '26.02.2026',
-        imgAlt: '',
-        title: 'Lorem ipsum dolor sit amet, consetetur sadipscing',
-        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-        buttonText: 'Lorem ipsum',
-        showButton: false,
-    },
-    {
-        imgSrc: '',
-        author: 'Leonidas Vanec, Ilse Bamberg',
-        date: '26.02.2026',
-        imgAlt: '',
-        title: 'Lorem ipsum dolor sit amet, consetetur sadipscing',
-        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-        buttonText: 'Lorem ipsum',
-        showButton: false,
-    },
-    {
-        imgSrc: '',
-        author: 'Leonidas Vanec, Ilse Bamberg',
-        date: '26.02.2026',
-        imgAlt: '',
-        title: 'Lorem ipsum dolor sit amet, consetetur sadipscing',
-        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-        buttonText: 'Lorem ipsum',
-        showButton: false,
-    },
-    {
-        imgSrc: '',
-        author: 'Leonidas Vanec, Ilse Bamberg',
-        date: '26.02.2026',
-        imgAlt: '',
-        title: 'Lorem ipsum dolor sit amet, consetetur sadipscing',
-        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-        buttonText: 'Lorem ipsum',
-        showButton: false,
-    },
-    {
-        imgSrc: '',
-        author: 'Leonidas Vanec, Ilse Bamberg',
-        date: '26.02.2026',
-        imgAlt: '',
-        title: 'Lorem ipsum dolor sit amet, consetetur sadipscing',
-        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-        buttonText: 'Lorem ipsum',
-        showButton: false,
-    },
-    {
-        imgSrc: '',
-        author: 'Leonidas Vanec, Ilse Bamberg',
-        date: '26.02.2026',
-        imgAlt: '',
-        title: 'Lorem ipsum dolor sit amet, consetetur sadipscing',
-        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-        buttonText: 'Lorem ipsum',
-        showButton: false,
-    },
-    {
-        imgSrc: '',
-        author: 'Leonidas Vanec, Ilse Bamberg',
-        date: '26.02.2026',
-        imgAlt: '',
-        title: 'Lorem ipsum dolor sit amet, consetetur sadipscing',
-        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-        buttonText: 'Lorem ipsum',
-        showButton: false,
-    },
-    {
-        imgSrc: '',
-        author: 'Leonidas Vanec, Ilse Bamberg',
-        date: '26.02.2026',
-        imgAlt: '',
-        title: 'Lorem ipsum dolor sit amet, consetetur sadipscing',
-        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-        buttonText: 'Lorem ipsum',
-        showButton: false,
     }
 ];
 jSecRenderer.initialize(SectionsMain);
